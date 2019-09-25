657076aa91f18524c567fe632a5a7e2499692eba
Andrew Whitehead <cywolf@gmail.com>
2019-09-25 13:25:31 -0700

    add tests to Conductor
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- aries_cloudagent/tests/test_conductor.py
+++ aries_cloudagent/tests/test_conductor.py
@@ -1,4 +1,5 @@
 import asyncio
+from io import StringIO
 from unittest import mock, TestCase
 from asynctest import TestCase as AsyncTestCase
 from asynctest import mock as async_mock
@@ -9,11 +10,16 @@ from .. import conductor as test_module
 from ..admin.base_server import BaseAdminServer
 from ..config.base_context import ContextBuilder
 from ..config.injection_context import InjectionContext
+from ..messaging.connections.manager import ConnectionManager
+from ..messaging.connections.models.connection_record import ConnectionRecord
 from ..messaging.connections.models.connection_target import ConnectionTarget
 from ..messaging.message_delivery import MessageDelivery
 from ..messaging.serializer import MessageSerializer
 from ..messaging.outbound_message import OutboundMessage
 from ..messaging.protocol_registry import ProtocolRegistry
+from ..stats import Collector
+from ..storage.base import BaseStorage
+from ..storage.basic import BasicStorage
 from ..transport.inbound.base import InboundTransportConfiguration
 from ..transport.outbound.queue.base import BaseOutboundMessageQueue
 from ..transport.outbound.queue.basic import BasicOutboundMessageQueue
@@ -80,9 +86,17 @@ class StubContextBuilder(ContextBuilder):
         context.injector.bind_instance(
             BaseOutboundMessageQueue, BasicOutboundMessageQueue()
         )
+        context.injector.bind_instance(BaseStorage, BasicStorage())
         context.injector.bind_instance(BaseWallet, BasicWallet())
         context.injector.bind_instance(ProtocolRegistry, ProtocolRegistry())
         context.injector.bind_instance(MessageSerializer, self.message_serializer)
+        return context
+
+
+class StubCollectorContextBuilder(StubContextBuilder):
+    async def build(self) -> InjectionContext:
+        context = await super().build()
+        context.injector.bind_instance(Collector, Collector())
         return context
 
 
@@ -129,7 +143,12 @@ class TestConductor(AsyncTestCase, Config, TestDIDs):
 
         await conductor.setup()
 
-        with async_mock.patch.object(conductor.dispatcher, "dispatch") as mock_dispatch:
+        with async_mock.patch.object(
+            conductor.dispatcher, "dispatch", autospec=True
+        ) as mock_dispatch:
+            dispatch_result = """{"@type": "..."}"""
+            mock_dispatch.return_value.return_value = asyncio.Future()
+            mock_dispatch.return_value.return_value.set_result(None)
 
             delivery = MessageDelivery()
             parsed_msg = {}
@@ -139,7 +158,8 @@ class TestConductor(AsyncTestCase, Config, TestDIDs):
 
             message_body = "{}"
             transport = "http"
-            await conductor.inbound_message_router(message_body, transport)
+            complete = await conductor.inbound_message_router(message_body, transport)
+            asyncio.wait_for(complete, 1.0)
 
             mock_serializer.parse_message.assert_awaited_once_with(
                 conductor.context, message_body, transport
@@ -148,6 +168,48 @@ class TestConductor(AsyncTestCase, Config, TestDIDs):
             mock_dispatch.assert_awaited_once_with(
                 parsed_msg, delivery, None, conductor.outbound_message_router
             )
+
+    async def test_direct_response(self):
+        builder: ContextBuilder = StubContextBuilder(self.test_settings)
+        conductor = test_module.Conductor(builder)
+
+        await conductor.setup()
+
+        single_response = asyncio.Future()
+        dispatch_result = """{"@type": "..."}"""
+
+        async def mock_dispatch(parsed_msg, delivery, connection, outbound):
+            socket_id = delivery.socket_id
+            socket = conductor.sockets[socket_id]
+            socket.reply_mode = "all"
+            reply = OutboundMessage(
+                dispatch_result,
+                connection_id=None,
+                encoded=False,
+                endpoint=None,
+                reply_socket_id=socket_id,
+            )
+            await outbound(reply)
+            result = asyncio.Future()
+            result.set_result(None)
+            return result
+
+        with async_mock.patch.object(conductor.dispatcher, "dispatch", mock_dispatch):
+
+            delivery = MessageDelivery()
+            parsed_msg = {}
+            mock_serializer = builder.message_serializer
+            mock_serializer.extract_message_type.return_value = "message_type"
+            mock_serializer.parse_message.return_value = (parsed_msg, delivery)
+
+            message_body = "{}"
+            transport = "http"
+            complete = await conductor.inbound_message_router(
+                message_body, transport, None, single_response
+            )
+            asyncio.wait_for(complete, 1.0)
+
+            assert single_response.result() == dispatch_result
 
     async def test_outbound_message_handler(self):
         builder: ContextBuilder = StubContextBuilder(self.test_settings)
@@ -270,3 +332,35 @@ class TestConductor(AsyncTestCase, Config, TestDIDs):
 
             await conductor.stop()
             admin_stop.assert_awaited_once_with()
+
+    async def test_setup_collector(self):
+        builder: ContextBuilder = StubCollectorContextBuilder(self.test_settings)
+        builder.update_settings(self.good_inbound_transports)
+        builder.update_settings(self.good_outbound_transports)
+        conductor = test_module.Conductor(builder)
+
+        with async_mock.patch.object(
+            test_module, "InboundTransportManager", autospec=True
+        ) as mock_inbound_mgr, async_mock.patch.object(
+            test_module, "OutboundTransportManager", autospec=True
+        ) as mock_outbound_mgr, async_mock.patch.object(
+            test_module, "LoggingConfigurator", autospec=True
+        ) as mock_logger:
+
+            await conductor.setup()
+
+    async def test_print_invite(self):
+        builder: ContextBuilder = StubContextBuilder(self.test_settings)
+        builder.update_settings(
+            {"debug.print_invitation": True, "invite_base_url": "http://localhost"}
+        )
+        conductor = test_module.Conductor(builder)
+
+        with mock.patch("sys.stdout", new=StringIO()) as captured:
+            await conductor.setup()
+
+            await conductor.start()
+
+            await conductor.stop()
+
+            assert "http://localhost?c_i=" in captured.getvalue()
