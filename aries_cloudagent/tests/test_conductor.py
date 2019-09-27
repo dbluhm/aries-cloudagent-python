15d2751873b42227ed7d978f8a87983729fdf524
Andrew Whitehead <cywolf@gmail.com>
2019-09-27 11:10:02 -0700

    fix test case
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- aries_cloudagent/tests/test_conductor.py
+++ aries_cloudagent/tests/test_conductor.py
@@ -309,7 +309,7 @@ class TestConductor(AsyncTestCase, Config, TestDIDs):
                 r_future = await conductor.inbound_message_router(
                     message_body, transport, single_response=delivery_future
                 )
-                r_future_result = await r_future  # required for test passing.
+                r_future_result = await r_future
                 mock_delivery_queue.return_value.has_message_for_key.assert_called_once_with(
                     sender_pk.value
                 )
